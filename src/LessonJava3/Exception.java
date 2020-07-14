3dee204c1b1d602ae5e7f872f45216f82d41f494
sherlockvarious <2737793330@qq.com>
2020-07-14 10:06:03 +0800

    1

--- src/LessonJava3/Exception.java
+++ src/LessonJava3/Exception.java
@@ -1,11 +1,10 @@
-package homework1;
+package LessonJava3;
 /*
  *@author 27377-sun chao
- *@date 2020/7/13
+ *@date 2020/7/14
  *@school  SiChuan University
  */
 
-public class Hello {
-
+public class Exception {
 
 }
